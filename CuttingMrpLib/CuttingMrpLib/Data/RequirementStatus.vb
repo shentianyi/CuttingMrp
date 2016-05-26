d05cf4a84f64b2e438942f385f44862f583eb86a
Tianyi Shen <tianyi.shen@cz-tek.com>
2016-05-26 17:48:36 +0800

    processorder

---
+++ CuttingMrpLib/CuttingMrpLib/Data/RequirementStatus.vb
@@ -0,0 +1,6 @@
+﻿Public Enum RequirementStatus
+    Open = 1000
+    CancelManual = 2000
+    CancelSystem = 3000
+    Close = 5000
+End Enum
---
+++ CuttingMrpLib/CuttingMrpLib/Service/IProcessOrderService.vb
@@ -0,0 +1,5 @@
+﻿Public Interface IProcessOrderService
+    Sub UpdateOrderQuantity(toUpdateId As String, quantity As Double)
+    Sub CancelOrdersByIds(ids As List(Of String), isSystem As Boolean)
+    Function GenerateProcessOrderByRequirement(requirements As List(Of Requirement), reserveType As List(Of String)) As Boolean
+End Interface
