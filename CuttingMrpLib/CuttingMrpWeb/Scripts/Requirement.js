7537d318a8899128bec4151f4c22ba0dfc513c8e
SongFuZhen <evil_song@126.com>
2016-05-30 10:25:56 +0800

    brower ui fixed

--- CuttingMrpLib/CuttingMrpWeb/Scripts/Requirement.js
+++ CuttingMrpLib/CuttingMrpWeb/Scripts/Requirement.js
@@ -40,7 +40,7 @@ Requirement.click_filter = function () {
 
 Requirement.add_string_label_to_div = function (content, name, cls) {
     if (content != null && content != "") {
-        $("<p class='label label-primary' style='margin-left:5px;font-size:.8em;'>" + name + " " + content + "</p>").appendTo(cls).ready(function () {
+        $("<p class='label label-primary' style='margin-left:5px;font-size:.8em;white-space:normal;'>" + name + " " + content + "</p>").appendTo(cls).ready(function () {
         });
     }
 }
@@ -49,13 +49,13 @@ Requirement.add_range_label_to_div = function (content, name, cls) {
     var from = content.split("~")[0];
     var to = content.split("~")[1];
     if ((from != "" && from != null) && (to != null && to != "")) {
-        $("<p class='label label-primary' style='margin-left:5px;font-size:.8em;'>" + name + " : " + from + "~" + to + "</p>").appendTo(cls).ready(function () {
+        $("<p class='label label-primary' style='margin-left:5px;font-size:.8em;white-space:normal;'>" + name + " : " + from + "~" + to + "</p>").appendTo(cls).ready(function () {
         });
     } else if ((from != "" && from != null) && (to == null || to == "")) {
-        $("<p class='label label-primary' style='margin-left:5px;font-size:.8em;'>" + name + ">=" + from + "</p>").appendTo(cls).ready(function () {
+        $("<p class='label label-primary' style='margin-left:5px;font-size:.8em;white-space:normal;'>" + name + ">=" + from + "</p>").appendTo(cls).ready(function () {
         });
     } else if ((from == "" || from == null) && (to != null && to != "")) {
-        $("<p class='label label-primary' style='margin-left:5px;font-size:.8em;'>" + name + "<=" + to + "</p>").appendTo(cls).ready(function () {
+        $("<p class='label label-primary' style='margin-left:5px;font-size:.8em;white-space:normal;'>" + name + "<=" + to + "</p>").appendTo(cls).ready(function () {
         });
     }
 }
