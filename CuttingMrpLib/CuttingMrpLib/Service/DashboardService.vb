8c52f54e117e37f886c0e3c265077b0504fc6dbb
Charlot <7wangsong@gmail.com>
2016-06-03 17:11:04 +0800

    on dash

--- CuttingMrpLib/CuttingMrpLib/Service/DashboardService.vb
+++ CuttingMrpLib/CuttingMrpLib/Service/DashboardService.vb
@@ -12,15 +12,15 @@ Public Class DashboardService
 
     Public Function GetPartCompleteRateDash(searchModel As DashboardSearchModel) As List(Of DashboardItem) Implements IDashboardService.GetPartCompleteRateDash
         Dim context As DataContext = New DataContext(Me.DBConn)
-        Dim rep As Repository(Of StockSumRecord) = New Repository(Of StockSumRecord)(context)
-        Dim stocks As List(Of StockSumRecord) = rep.FindAll(Function(c) c.partNr.Equals(searchModel.PartNr) And c.date >= searchModel.DateFrom And c.date <= searchModel.DateTo).ToList
+        Dim rep As Repository(Of AvgOfCompleteRate) = New Repository(Of AvgOfCompleteRate)(context)
+        Dim rates As List(Of AvgOfCompleteRate) = rep.FindAll(Function(c) c.partNr.Equals(searchModel.PartNr) And c.proceeDate >= searchModel.DateFrom And c.proceeDate <= searchModel.DateTo).ToList
         Dim items As List(Of DashboardItem) = New List(Of DashboardItem)
         Dim d As DateTime = searchModel.DateFrom
 
-        While d <= searchModel.DateTo
-            Dim stock = stocks.Where(Function(s) s.date.Equals(d)).FirstOrDefault
-            items.Add(New DashboardItem() With {.XValue = d.Date.ToString, .YValue = If(stock Is Nothing, 0, stock.ToString)})
-        End While
+        For Each r In rates
+            items.Add(New DashboardItem() With {.XValue = r.proceeDate.Date.ToString, .YValue = r.rate})
+        Next
+
         Return items
     End Function
 
@@ -33,6 +33,7 @@ Public Class DashboardService
         While d <= searchModel.DateTo
             Dim stock = stocks.Where(Function(s) s.date.Equals(d)).FirstOrDefault
             items.Add(New DashboardItem() With {.XValue = d.Date.ToString, .YValue = If(stock Is Nothing, 0, stock.ToString)})
+            d = d.AddDays(1)
         End While
         Return items
     End Function
