64332bb4fd1e637d1971c6afe03d1551e3eb6937
Charlot <7wangsong@gmail.com>
2016-06-03 17:38:50 +0800

    add dash api

--- CuttingMrpLib/CuttingMrpLib/ViewModel/SearchViewModel/DashboardSearchModel.vb
+++ CuttingMrpLib/CuttingMrpLib/ViewModel/SearchViewModel/DashboardSearchModel.vb
@@ -2,4 +2,5 @@
     Public Property PartNr As String
     Public Property DateFrom As DateTime
     Public Property DateTo As DateTime
+    Public Property Type As Integer
 End Class
--- CuttingMrpLib/CuttingMrpWeb/Controllers/DashboardController.cs
+++ CuttingMrpLib/CuttingMrpWeb/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CuttingMrpLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -19,6 +20,24 @@ namespace CuttingMrpWeb.Controllers
             return View();
         }
 
+        [HttpGet]
+        public JsonResult Data([Bind(Include ="PartNr,DateFrom,DateTo,Type")] DashboardSearchModel searchModel) {
+            IDashboardService ds = new DashboardService(Properties.Settings.Default.db);
 
+            List<DashboardItem> items = new List<DashboardItem>();
+            switch (searchModel.Type)
+            {
+                case 100:
+                    items = ds.GetPartStockDash(searchModel);
+                    break;
+                case 200:
+                    items = ds.GetPartCompleteRateDash(searchModel);
+                    break;
+                default:
+                    break;
+            }
+            return Json(items, JsonRequestBehavior.AllowGet);
+
+        }
     }
 }
