959b9205eb5b5de98c3142d8fd46d82944cc6f9c
Charlot <7wangsong@gmail.com>
2016-06-03 14:19:54 +0800

    add dash svc

---
+++ CuttingMrpLib/CuttingMrpDashSvc/DashService.cs
@@ -0,0 +1,33 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace CuttingMrpDashSvc
+{
+    public partial class DashService : ServiceBase
+    {
+        ILog log = LogManager.GetLogger("ServiceLog");
+        public DashService()
+        {
+            InitializeComponent();
+            log4net.Config.XmlConfigurator.ConfigureAndWatch(new FileInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log4.config")));
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            log.Info("CZ CuttingMrpDash Start!");
+        }
+
+        protected override void OnStop()
+        {
+            log.Info("CZ CuttingMrpDash Stop!");
+        }
+    }
+}
---
+++ CuttingMrpLib/CuttingMrpDashSvc/install.bat
@@ -0,0 +1,2 @@
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe C:\cz\MrpDashSvc\CuttingMrpDashSvc.exe
+pause
