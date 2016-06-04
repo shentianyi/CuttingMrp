d04e7e14f8fe7d92bf4b388a1a331ed679bed16d
Charlot <7wangsong@gmail.com>
2016-06-04 18:29:17 +0800

    add movement

---
+++ CuttingMrpLib/CuttingMrpLib/Model/StockMovementPartial.vb
@@ -0,0 +1,20 @@
+﻿Partial Public Class StockMovement
+    Public ReadOnly Property createdAtDisplay As String
+        Get
+            Return Me.createdAt.Value.ToString()
+        End Get
+    End Property
+
+    Public ReadOnly Property fifoDisplay As String
+        Get
+            Return Me.fifo.ToString()
+        End Get
+    End Property
+
+    Public ReadOnly Property typeDisplay As String
+        Get
+            Return EnumUtility.GetDescription(DirectCast(Me.moveType, StockMoveType))
+        End Get
+    End Property
+
+End Class
