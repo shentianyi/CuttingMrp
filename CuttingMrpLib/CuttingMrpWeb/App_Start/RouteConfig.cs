92925a975ec7b74f63bde355e893a5dba73fed69
SongFuZhen <evil_song@126.com>
2016-06-05 14:02:19 +0800

    home page change to dashboard

--- CuttingMrpLib/CuttingMrpWeb/App_Start/RouteConfig.cs
+++ CuttingMrpLib/CuttingMrpWeb/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CuttingMrpWeb
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "ProcessOrders", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Dashboard", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
